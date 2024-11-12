6d7786dd58b21be107a88fdb41ffa367d0d017ad
Rayane <rayanetoko@gmail.com>
2024-11-13 00:47:38 +0100

    feat(header navigation): update header navigation buttons style when the user is not logged in

--- src/components/Header/SignedOutNav.tsx
+++ src/components/Header/SignedOutNav.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 
 function SignedOutNav() {
+  const tailwindClasses = 'text-white py-2 px-4 rounded transition duration-300 hover:bg-[#4a4eec]';
+
   return (
     <ul className='flex gap-4 items-center'>
-      <li><Link href='/signup'>Sign up</Link></li>
-      <li><Link href='/signin'>Sign In</Link></li>
+      <li><Link href='/signup' className={tailwindClasses}>Sign up</Link></li>
+      <li><Link href='/signin' className={tailwindClasses}>Sign In</Link></li>
     </ul>
   )
 }
