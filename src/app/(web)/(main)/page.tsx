98b2125a13ddf783505a1e97205b3532ba80be1e
Rayane <rayanetoko@gmail.com>
2024-11-13 00:45:21 +0100

    feat(homepage): update homepage design

--- src/app/(web)/(main)/page.tsx
+++ src/app/(web)/(main)/page.tsx
@@ -1,16 +1,61 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 
 export default function Home() {
 
 	return (
 		<main className={styles.main}>
-			<h1 className={styles.headline}>It more than just a trip</h1>
-				<div className={styles.hero} />
-				<div className={styles.buttonWrapper}>
-					<Link href="/signup">Get started</Link>
+			<h1 className={styles.headline}>It&apos;s more than just a trip</h1>
+			<div className={styles.gallery}>
+				<div>
+					<Image
+						alt=""
+						src="/images/home-4.jpg"
+						fill
+						objectFit="cover"
+					/>
+					<Image
+						alt=""
+						src="/images/home-5.jpg"
+						fill
+						objectFit="cover"
+					/>
 				</div>
+				<div>
+					<Image
+						alt=""
+						src="/images/home-2.jpg"
+						fill
+						objectFit="cover"
+					/>
+					<Image
+						alt=""
+						src="/images/home-3.jpg"
+						fill
+						objectFit="cover"
+					/>
+				</div>
+				<div>
+					<Image
+						alt=""
+						src="/images/home-3.jpg"
+						fill
+						objectFit="cover"
+					/>
+					<Image
+						alt=""
+						src="/images/home-2.jpg"
+						fill
+						objectFit="cover"
+					/>
+				</div>
+			</div>
+			<h2 className={styles.headline}>It&apos;s a way to share memories</h2>
+			<div className={styles.buttonWrapper}>
+				<Link href="/signup">Get started</Link>
+			</div>
 		</main>
 	);
 }
